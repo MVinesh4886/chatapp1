50ef6def751223e33b08670050f17277aa2cf423
vinesh <vineshkrishna26@gmail.com>
2023-12-20 22:15:02 +0530

    updated backend

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
+require("dotenv").config();
+const db = require("./config/database");
+const User = require("../chatapp/model/User");
+const userRoute = require("../chatapp/route/userRoute");
 const express = require("express");
+
 const app = express();
 
-const PORT = 3000;
+app.use(express.json());
 
-app.listen(PORT, () => {
-  console.log(`Server is listening on ${PORT}`);
-});
+app.use("/user", userRoute);
+
+const PORT = process.env.PORT;
+
+db.sync()
+  .then((result) => {
+    // console.log(result);
+    app.listen(PORT, () => {
+      console.log(`Server is listening on ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
---
+++ config/database.js
@@ -0,0 +1,17 @@
+const Sequelize = require("sequelize");
+
+const db = new Sequelize("chatapp", "root", process.env.PASSWORD_KEY, {
+  host: "localhost",
+  dialect: "mysql",
+});
+
+(async () => {
+  try {
+    await db.authenticate();
+    console.log("Database connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+})();
+
+module.exports = db;
