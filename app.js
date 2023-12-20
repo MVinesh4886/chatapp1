841641cb4ea49cd0a53fd0b03439e9362d9ca0c4
vinesh <vineshkrishna26@gmail.com>
2023-12-20 23:04:58 +0530

    backend-frontend

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ const db = require("./config/database");
 const User = require("../chatapp/model/User");
 const userRoute = require("../chatapp/route/userRoute");
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
-
+app.use(
+  cors({
+    origin: "http://127.0.0.1:5500",
+    methods: ["GET", "POST", "DELETE", "PUT"],
+  })
+);
 app.use("/user", userRoute);
 
 const PORT = process.env.PORT;
